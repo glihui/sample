0325ce017a74843bca13b5324dcc973c9bd1ce5e
guolihui <15919476778@163.com>
2018-04-12 01:58:26 +0000

    Add user statuses

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -6,7 +6,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">重置密码</div>
                 <div class="panel-body">
-                    @if (session('status'))
+           sasasa         @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
